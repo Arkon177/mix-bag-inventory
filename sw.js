1553ef0eea4273225c182c602d57280bf5c6ce82
Arkon177 <misshopemills@gmail.com>
2026-05-09 20:02:31 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Mix Bag Inventory
 // Enables offline functionality and caching
 
-const CACHE_NAME = 'mixbag-v2.2.0';
+const CACHE_NAME = 'mixbag-v2.3.0';
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
